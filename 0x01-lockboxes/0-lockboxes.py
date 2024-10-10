e0cb9d7b7b7930939c135f7f4e8d356a1fe4cb48
Chirchir-Dan <1danchirchir@gmail.com>
2024-10-10 18:57:29 +0300

    Fixed pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Module to determine if all boxes can be unlocked.
 
-This module contains a function, `canUnlockAll`, which checks if all 
-locked boxes can be opened given the keys inside each box. The first box 
+This module contains a function, `canUnlockAll`, which checks if all
+locked boxes can be opened given the keys inside each box. The first box
 is assumed to be unlocked at the start.
 
 Function:
-    canUnlockAll(boxes): Returns True if all boxes can be opened, 
+    canUnlockAll(boxes): Returns True if all boxes can be opened,
                          otherwise returns False.
 """
+
 
 def canUnlockAll(boxes):
     """
     Determine if all boxes can be unlocked.
 
-    Each box is represented as a list of keys, where a key with the same 
+    Each box is represented as a list of keys, where a key with the same
     number as a box opens that box.
 
     Parameters:
-    boxes (list of list): A list of lists where each sublist contains 
+    boxes (list of list): A list of lists where each sublist contains
                           the keys that can unlock other boxes.
 
     Returns:
@@ -31,7 +32,7 @@ def canUnlockAll(boxes):
 
     while queue:
         current_box = queue.pop(0)  # Get the next box to unlock
-        for key in boxes[current_box]:  # Iterate over the keys in the current box
+        for key in boxes[current_box]:  # Iterate over the keys in current box
             if key < len(boxes) and not unlocked[key]:  # Validate key
                 unlocked[key] = True  # Mark the box as unlocked
                 queue.append(key)  # Add the unlocked box to the queue
